3239b622bf1e9a8083a95e71338107819b1f574d
Sridhar200406 <yadlapallisridhar1@gmail.com>
2026-06-17 13:25:37 +0530

    17 th day progress by sridhar

--- week-1/Algorithms_Data Structures_ecommerce_search/main.py
+++ week-1/Algorithms_Data Structures_ecommerce_search/main.py
@@ -3,3 +3,4 @@ from search import linear_search,binary_search
 products=[Product(101,'Camera'),Product(102,'Keyboard'),Product(103,'Laptop'),Product(104,'Mouse'),Product(105,'Phone')]
 print(linear_search(products,'Laptop'))
 print(binary_search(products,'Laptop'))
+// this is a project of sridhar's 
